920624b7d39494bd1a7cf4fb1f5c6ce9a4d8ddbb
Maciej Lasota <maciej.lasota@j-labs.pl>
2022-06-28 13:16:48 +0200

    Source code added

---
+++ src/test/java/it/lasota/chava/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lasota.chava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
